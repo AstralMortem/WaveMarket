eb8621882453e7471d26d53a8a055e80eae6beb3
Vladyslav Chaliuk <vladchaluk@gmail.com>
2023-03-28 18:07:29 +0300

    updated storage

--- core/settings.py
+++ core/settings.py
@@ -182,5 +182,8 @@ EMAIL_HOST_USER = 'wavemarketua@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASSWORD")
 
 
-DEFAULT_FILE_STORAGE = "storages.backends.dropbox.DropBoxStorage"
-DROPBOX_OAUTH2_TOKEN = os.environ.get('DROPBOX_STORAGE')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'wavemarket'
+AWS_QUERY_STRING = False
